e9701339fb6f8e970b29848725a526781c78896d
selen-klblyva <kalbaliyevaselen@gmail.com>
2025-08-18 16:33:21 +0400

    Updated ClinicNotFoundException.java

--- panacea/src/main/java/com/javidkhalilov/panacea/exception/ClinicNotFoundException.java
+++ panacea/src/main/java/com/javidkhalilov/panacea/exception/ClinicNotFoundException.java
@@ -1,20 +1,22 @@
 package com.javidkhalilov.panacea.exception;
 
 import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@RequiredArgsConstructor
 public class ClinicNotFoundException extends RuntimeException {
-    final Long clinicId;
+    private final Long clinicId;
 
-    public ClinicNotFoundException(Long clinicId) {
-        super("Clinic not found with id: " + clinicId);
-        this.clinicId = clinicId;
-    }
-    public Long getClinicId() {
-        return clinicId;
+    @Override
+    public String getMessage() {
+        return "Clinic not found with id: " + clinicId;
     }
 }
+
